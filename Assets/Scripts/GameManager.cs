8743a634dac5344d8499aa4c8667c3062d4a90e6
Lachlan Richardson <lachlan.richard@ad.aie.edu>
2016-06-20 15:14:35 +1000

    Added scene top collider

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour {
 
 	//Make private / hidden towards the end
 	public int BallsRemaining = 6;
+
+    //BallPrefab
+    public GameObject BallPrefab;
 	//BallScript
 
 	// Use this for initialization
@@ -18,7 +21,9 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-
+        //test button
+        if (Input.GetKeyDown(KeyCode.W))
+            ResetBall();
 	}
 
 	public void UpdateScore(int Score)
@@ -29,4 +34,14 @@ public class GameManager : MonoBehaviour {
 	{
 		ScoreTotal = 0;
 	}
+    public void ResetBall()
+    {
+        if (BallsRemaining > 0)
+        {
+            //resetPos;
+            BallPrefab.transform.position = new Vector3(4.427f, -1.514f, 2.368f);
+            BallPrefab.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            BallsRemaining--;
+        }
+    }
 }
