18eea5fc16dd71b9c59a1d23c31f405a2276359e
Sean Mauff <sjmauff@hotmail.com>
2016-06-20 13:37:35 +1000

    Committing all the things I missed prior

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour {
+    public int m_bumperForce;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+       coll.rigidbody.AddForce(Vector3.Normalize(coll.transform.position - transform.position) * m_bumperForce, ForceMode.Impulse);
+    }
+}
