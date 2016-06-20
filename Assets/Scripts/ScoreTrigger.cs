efb7fae13c070e6848340f4dcf85b0b5b0947ad0
Lachlan Richardson <lachlan.richard@ad.aie.edu>
2016-06-20 12:00:52 +1000

    Scripts Upload

---
+++ Assets/Scripts/ScoreTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreTrigger : MonoBehaviour {
+
+	public int ScoreValue;
+
+	public GameManager GM;
+	// Use this for initialization
+	void Start ()
+	{
+		ScoreValue = 100;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		GM.UpdateScore(ScoreValue);
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		GM.UpdateScore(ScoreValue);
+	}
+}
