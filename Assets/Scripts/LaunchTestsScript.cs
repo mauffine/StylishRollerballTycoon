b86598b2c2b9fbc82796cb1ac23c790f9ac75e54
Sean Mauff <sjmauff@hotmail.com>
2016-06-20 12:06:31 +1000

    no message

---
+++ Assets/Scripts/LaunchTestsScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchTestsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(-0.8f, 0.2f, 0) * 13, ForceMode.Impulse);
+        }
+	}
+}
