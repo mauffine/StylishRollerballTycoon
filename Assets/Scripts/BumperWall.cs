90f28805db68e0a9643fb95d94fb593c9415d13f
Sean Mauff <sjmauff@hotmail.com>
2016-06-20 15:16:10 +1000

    Fixed bumper walls for above the paddles

---
+++ Assets/Scripts/BumperWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumperWall : MonoBehaviour
+{
+    public float m_bumperForce;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+
+        coll.rigidbody.AddForce(Vector3.Normalize(coll.impulse) * m_bumperForce, ForceMode.Impulse);
+        //coll.rigidbody.AddForce(Vector3.Normalize(coll.transform.position - transform.position) * m_bumperForce, ForceMode.Impulse);
+    }
+}
